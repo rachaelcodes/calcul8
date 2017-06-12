4503c4cff5b7e47a830a78db9f19ab54ddf05e54
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-06-12 21:39:34 +0100

    added output and troubleshooted input

--- app.js
+++ app.js
@@ -1,47 +1,69 @@
-var calculation = [];
-
+let calculation = [];
+let number = '';
 document.getElementById('1').addEventListener('click', function(event) {
-    calculation.push('1');
+    number+='1';
   }, false);
   document.getElementById('2').addEventListener('click', function(event) {
-    calculation.push('2');
+    number+='2';
   }, false);
 document.getElementById('3').addEventListener('click', function(event) {
-    calculation.push('3');
+    number+='3';
   }, false);
 document.getElementById('4').addEventListener('click', function(event) {
-    calculation.push('4');
+    number+='4';
   }, false);
 document.getElementById('5').addEventListener('click', function(event) {
-    calculation.push('5');
+    number+='5';
   }, false);
 document.getElementById('6').addEventListener('click', function(event) {
-    calculation.push('6');
+    number+='6';
   }, false);
 document.getElementById('7').addEventListener('click', function(event) {
-    calculation.push('7');
+    number+='7';
   }, false);
 document.getElementById('8').addEventListener('click', function(event) {
-    calculation.push('8');
+    number+='8';
   }, false);
 document.getElementById('9').addEventListener('click', function(event) {
-    calculation.push('9');
+    number+='9';
   }, false);
 document.getElementById('0').addEventListener('click', function(event) {
-    calculation.push('0');
+    number+='0';
   }, false);
+document.getElementById('point').addEventListener('click', function(event) {
+    number+='.';
+  }, false);
+
 document.getElementById('plus').addEventListener('click', function(event) {
-    calculation.push('+');
+  if (number){
+      calculation.push(number);
+      calculation.push('+');
+      number = '';
+      }
   }, false);
 document.getElementById('minus').addEventListener('click', function(event) {
-    calculation.push('-');
+  if (number){
+      calculation.push(number);
+      calculation.push('-');
+      number = '';
+      }
   }, false);
 document.getElementById('multiply').addEventListener('click', function(event) {
-    calculation.push('*');
+  if (number){
+      calculation.push(number);
+      calculation.push('*');
+      number = '';
+      }
   }, false);
 document.getElementById('divide').addEventListener('click', function(event) {
-    calculation.push('/');
+  if (number){
+      calculation.push(number);
+      calculation.push('/');
+      number = '';
+      }
   }, false);
 document.getElementById('equals').addEventListener('click', function(event) {
-      alert(calculation);
+      calculation.push(number);
+      document.getElementById('input').appendChild(document.createTextNode(calculation));
+      document.getElementById('answer').appendChild(document.createTextNode('ans'));
     }, false);
