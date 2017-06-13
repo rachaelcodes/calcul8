3e283d738de31dda27055af26e821fbeca168ebe
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-06-13 21:02:40 +0100

    calculation function created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let calculation = [];
 let number = '';
+let answer = 0;
 document.getElementById('1').addEventListener('click', function(event) {
     number+='1';
   }, false);
@@ -36,34 +37,66 @@ document.getElementById('point').addEventListener('click', function(event) {
 
 document.getElementById('plus').addEventListener('click', function(event) {
   if (number){
-      calculation.push(number);
+      calculation.push(parseFloat(number));
       calculation.push('+');
+      document.getElementById('input').appendChild(document.createTextNode(number + "+"));
       number = '';
       }
   }, false);
 document.getElementById('minus').addEventListener('click', function(event) {
   if (number){
-      calculation.push(number);
+      calculation.push(parseFloat(number));
       calculation.push('-');
+      document.getElementById('input').appendChild(document.createTextNode(number + "-"));
       number = '';
       }
   }, false);
 document.getElementById('multiply').addEventListener('click', function(event) {
   if (number){
-      calculation.push(number);
+      calculation.push(parseFloat(number));
       calculation.push('*');
+      document.getElementById('input').appendChild(document.createTextNode(number + "x"));
       number = '';
       }
   }, false);
 document.getElementById('divide').addEventListener('click', function(event) {
   if (number){
-      calculation.push(number);
+      calculation.push(parseFloat(number));
       calculation.push('/');
+      document.getElementById('input').appendChild(document.createTextNode(number + '\u00f7' ));
       number = '';
       }
   }, false);
 document.getElementById('equals').addEventListener('click', function(event) {
-      calculation.push(number);
-      document.getElementById('input').appendChild(document.createTextNode(calculation));
-      document.getElementById('answer').appendChild(document.createTextNode('ans'));
+    document.getElementById('input').appendChild(document.createTextNode(number));
+      calculation.push(parseFloat(number));
+      console.log(calculation);
+      calc(calculation);
+      //document.getElementById('input').appendChild(document.createTextNode(calculation));
+      document.getElementById('answer').appendChild(document.createTextNode(answer));
     }, false);
+
+function calc(array) {
+  for(var i = 0; i<array.length; i++){
+    if(array[i]=='*'){
+      array.splice(i-1, 3, (array[i-1]*array[i+1]));
+      i--;
+    }
+    else if(array[i]=='/'){
+      array.splice(i-1, 3, (array[i-1]/array[i+1]));
+      i--;
+    }
+  }
+  for(var j = 0; j<array.length; j++){
+    if(array[j]=='+'){
+      array.splice(j-1, 3, (array[j-1]+array[j+1]));
+      j--;
+    }
+    else if(array[j]=='-'){
+      array.splice(j-1, 3, (array[j-1]-array[j+1]));
+      j--;
+    }
+  }
+  answer = array;
+  return answer;
+}
